e64d90356f538d8a2b67e40f448e0a17428b247b
Francisco Bezerra <chagasfranciscobezerra88@gmail.com>
2022-06-20 09:46:08 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        //Como o usuário pode ter nenhum ou mais de 1 pedido adiciona-se uma coleção de pedidos
+        public ICollection<Pedido> Pedidos { get; set; } 
+
+    }
+}
